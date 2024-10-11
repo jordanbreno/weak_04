f79ae0fc266192117e10c7b3d89e0106978af57b
jordanbreno <jordanbreno20@gmail.com>
2024-10-11 12:05:29 -0300

    Add files via upload

---
+++ schemas/produtos.schema.js
@@ -0,0 +1,10 @@
+module.exports = {
+    type: "object",
+    properties: {
+        nome: {type: "string"},
+        descricao: {type: "string"},
+        preco: {type: "number"}
+    },
+    required: ["nome", "descricao", "preco"],
+    additionalProperties: false
+}
